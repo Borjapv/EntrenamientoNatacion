76decefb973a2b1ec550a0f776d935a3b8f8e7fb
Borja <borjapv20@gmail.com>
2021-11-02 14:28:14 +0100

    Created Medidas

---
+++ Core/Medidas.cs
@@ -0,0 +1,18 @@
+﻿namespace EntrenamientoNatacion.Core
+{
+    public class Medidas
+    {
+        private int peso { get; set; }
+        private int circAbd { get; set; }
+        private string notas { get; set; }
+
+        public Medidas(int peso, int circAbd, string notas)
+        {
+            this.peso = peso;
+            this.circAbd = circAbd;
+            this.notas = notas;
+        }
+        
+        
+    }
+}
