9a23653e7799f9233766ccb9181058c13e2bc1d5
Borja <borjapv20@gmail.com>
2021-11-16 12:43:12 +0100

    DataGrid in progress (2)

--- UI/DataMedidas.axaml.cs
+++ UI/DataMedidas.axaml.cs
@@ -18,10 +18,8 @@ namespace EntrenamientoNatacion.UI
             
             ListaMedidas lista = ListaMedidas.CargarDatos();
             var dataMedidas = this.FindControl<DataGrid>( "DataMedidas" );
-            Items = lista.GetLista();
+            dataMedidas.Items = lista.GetLista();
         }
-
-        public Dictionary<DateTime, Medidas> Items { get; set; }
 
         private void InitializeComponent()
         {
